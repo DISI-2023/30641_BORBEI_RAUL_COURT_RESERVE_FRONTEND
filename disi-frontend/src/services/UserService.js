54fa056cc6dbe569f40943f5c9c2978afb4ea20a
Paula Costea <Costea.Pa.Paula@student.utcluj.ro>
2023-04-13 20:02:15 +0300

    edited credentials

--- disi-frontend/src/services/UserService.js
+++ disi-frontend/src/services/UserService.js
@@ -1,13 +1,13 @@
 import axiosInstance from "../axios";
 
 function RegisterClient(email, username, password) {
-    let credentilas = {
+    let credentials = {
         email: email,
         username: username,
         password: password,
     }
 
-    axiosInstance.post("/user", credentilas)
+    axiosInstance.post("/user", credentials)
         .then(
             res => {
             }
