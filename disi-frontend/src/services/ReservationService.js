7d4e736a0bdf8e3a415de0aaa52c2eca9a90298d
Savi <savilitu@yahoo.com>
2023-05-05 16:05:22 +0300

    Added cancel reservation functionality

--- disi-frontend/src/services/ReservationService.js
+++ disi-frontend/src/services/ReservationService.js
@@ -54,3 +54,17 @@ export function GetUserReservations(userId, callback, errorCallback) {
         })
 }
 
+export function CancelReservation(id, callback, errorCallback) {
+    axiosInstance.delete("/reservation/" + id)
+        .then(
+            res => {
+                if (callback != null)
+                    callback(res)
+            }
+        )
+        .catch(error => {
+            if (errorCallback != null)
+                errorCallback(error)
+        })
+}
+
