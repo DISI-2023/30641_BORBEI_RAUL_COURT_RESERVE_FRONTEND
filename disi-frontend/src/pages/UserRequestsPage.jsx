5127759a347de872d38981dbc755fea4eeaf2787
Paula Costea <Costea.Pa.Paula@student.utcluj.ro>
2023-05-09 16:44:44 +0300

    Added search partner feature

--- disi-frontend/src/pages/UserRequestsPage.jsx
+++ disi-frontend/src/pages/UserRequestsPage.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { GetUserSubscriptions } from '../services/SubscriptionService';
-import { Accordion, Typography, AccordionSummary, AccordionDetails, Snackbar, Alert } from '@mui/material'
+import { GetRequests, AcceptRequest } from '../services/RequestService';
+import { Accordion, Typography, AccordionSummary, AccordionDetails, Snackbar, Alert, Button } from '@mui/material'
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Box from '@mui/material/Box';
@@ -9,7 +10,7 @@ import moment from 'moment/moment'
 
 function UserRequestsPage() {
     const userId = localStorage.getItem("id")
-    const [userSubscriptions, setUserSubscriptions] = useState([])
+    const [userRequests, setUserRequests] = useState([])
     const [value, setValue] = React.useState('future');
 
     const handleChange = (event, newValue) => {
@@ -22,53 +23,86 @@ function UserRequestsPage() {
         return moment(date).format(format)
     }
 
+    const formatTime = (dateTime) => {
+        var format = "HH:mm"
+        var date = new Date(dateTime)
+        return moment(date).format(format)
+    }
+
     useEffect(() => {
-        GetUserSubscriptions(userId, (res) => {
-            setUserSubscriptions(res.data)
+        GetRequests(userId, (res) => {
+            setUserRequests(res.data)
             console.log(res.data)
         }, (err) => {
             console.log(err)
         })
     }, [])
 
-    function UsersSubscriptionsList(props) {
+    function UsersRequestsList(props) {
         return (
             <div>
                 {
-                    props.userSubscriptions?.map((subscription) => (
+                    props.userRequests?.map((request) => (
                         <Accordion>
                             <AccordionSummary
                                 expandIcon={<ChevronDown />}
-                                aria-controls={"subcription-" + subscription.id + "-content"}
-                                id={"subscriprion-" + subscription.id + "-header"}
+                                aria-controls={"request-" + request.id + "-content"}
+                                id={"request-" + request.id + "-header"}
                             >
                                 <Typography sx={{ fontWeight: "bolder", fontStyle: "italic", fontSize: "20px" }}>
-                                    {"Subscription for " + subscription.fieldName + " on " + formatDate(subscription.startTime)}
+                                    {"Request from " + request.postedByUser.email + " on " + formatDate(request.reservation.startTime)}
                                 </Typography>
                             </AccordionSummary>
                             <AccordionDetails>
                                 <div style={{ display: "flex", flexDirection: "column", gap: "10px", padding: "20px" }}>
+                                    {
+                                        request.takenByUser !== null ? (<div style={{ display: "inline-flex", gap: "20px" }}>
+                                            <Typography sx={{ fontWeight: "bolder", fontStyle: "italic" }}>Taken by</Typography><Typography>{request.takenByUser.username}</Typography>
+                                        </div>) : (<div style={{ display: "inline-flex", gap: "20px" }}>
+                                            <Typography sx={{ fontWeight: "bolder", fontStyle: "italic" }}>Taken by</Typography><Typography>{"-"}</Typography>
+                                        </div>)
+                                    }
                                     <div style={{ display: "inline-flex", gap: "20px" }}>
                                         <Typography sx={{ fontWeight: "bolder", fontStyle: "italic" }}>
                                             Field name
                                         </Typography>
                                         <Typography>
-                                            {subscription.fieldName}
+                                            {request.reservation.fieldName}
                                         </Typography>
                                     </div>
                                     <div style={{ display: "inline-flex", gap: "20px" }}>
-                                        <Typography sx={{ fontWeight: "bolder", fontStyle: "italic" }}>Starts on</Typography><Typography>{formatDate(subscription.startTime)}</Typography>
-                                        <Typography sx={{ fontWeight: "bolder", fontStyle: "italic" }}>at</Typography><Typography>{subscription.startHour}</Typography>
+                                        <Typography sx={{ fontWeight: "bolder", fontStyle: "italic" }}>Starts on</Typography><Typography>{formatDate(request.reservation.startTime)}</Typography>
+                                        <Typography sx={{ fontWeight: "bolder", fontStyle: "italic" }}>at</Typography><Typography>{formatTime(request.reservation.startTime)}</Typography>
                                     </div>
                                     <div style={{ display: "inline-flex", gap: "20px" }}>
-                                        <Typography sx={{ fontWeight: "bolder", fontStyle: "italic" }}>Ends on</Typography><Typography>{formatDate(subscription.endTime)}</Typography>
-                                        <Typography sx={{ fontWeight: "bolder", fontStyle: "italic" }}>at</Typography><Typography>{subscription.endHour}</Typography>
+                                        <Typography sx={{ fontWeight: "bolder", fontStyle: "italic" }}>Ends on</Typography><Typography>{formatDate(request.reservation.endTime)}</Typography>
+                                        <Typography sx={{ fontWeight: "bolder", fontStyle: "italic" }}>at</Typography><Typography>{formatTime(request.reservation.endTime)}</Typography>
                                     </div>
                                     <div style={{ display: "inline-flex", gap: "20px" }}>
-                                        <Typography sx={{ fontWeight: "bolder", fontStyle: "italic" }}>Type</Typography><Typography>{subscription.type}</Typography>
+                                        <Typography sx={{ fontWeight: "bolder", fontStyle: "italic" }}>Total</Typography><Typography>{request.reservation.finalPrice}</Typography>
                                     </div>
-                                    <div style={{ display: "inline-flex", gap: "20px" }}>
-                                        <Typography sx={{ fontWeight: "bolder", fontStyle: "italic" }}>Total</Typography><Typography>{subscription.finalPrice}</Typography>
+                                    <div style={{ display: "flex", gap: 12 }}>
+                                        <Button
+                                            variant="contained"
+                                            style={{
+                                                color: "white",
+                                                backgroundColor: "primary",
+                                            }}
+                                            disabled={request.takenByUser !== null}
+                                            onClick={() => {
+                                                AcceptRequest(request.id, 'false', request.postedByUser.id, localStorage.getItem("id"), request.reservation.id, (res) => {
+                                                    if (res.status === 200) {
+                                                        window.location.reload()
+                                                    }
+                                                }, (err) => {
+                                                })
+                                            }}
+                                            sx={{
+                                                width: "fit-content",
+                                            }}
+                                        >
+                                            Accept
+                                        </Button>
                                     </div>
                                 </div>
                             </AccordionDetails>
@@ -87,9 +121,10 @@ function UserRequestsPage() {
             <br></br>
             <h1>My requests</h1>
             <br></br>
+            <br></br>
             <Box sx={{ width: '100%' }}>
                 <Box>
-                    <UsersSubscriptionsList userSubscriptions={userSubscriptions.filter(subscription => {
+                    <UsersRequestsList userRequests={userRequests.filter(subscription => {
                         return subscription;
                     })} />
                 </Box>
